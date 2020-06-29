42ac2a41c795a149eacd85a01105185e994df2c5
SpessotoVitor <spessoto97@gmail.com>
2020-06-29 18:59:45 -0300

    Updating data using Realm

--- Todoey/features/Controller/TodoListViewController.swift
+++ Todoey/features/Controller/TodoListViewController.swift
@@ -65,13 +65,17 @@ class TodoListViewController: UITableViewController {
     // MARK: - TableView Delegate Methods
     //*************************************************
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        let item = todoItems?[indexPath.row]
-        
-//        context.delete(indexPath.row)
-//        itemsArray.remove(at: indexPath.row)
-        
-//        item?.done = !item?.done
-//        saveItems()
+        if let item = todoItems?[indexPath.row] {
+            do {
+                try realm.write {
+                    item.done = !item.done
+                    realm.add(item)
+                }
+            } catch {
+                print("Something went wrong while saving the item! --> \(error)")
+            }
+            tableView.reloadData()
+        }
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
